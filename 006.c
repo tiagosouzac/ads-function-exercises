cf0dd848ece23f8de9e89fd7c49a0789d2c96c95
Tiago Castro <tiagocastro602@gmail.com>
2023-11-25 14:23:00 -0300

    006

---
+++ 006.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int shape_type(int b, int h) { return b == h; }
+
+int main() {
+  int b, h, result;
+  scanf("%d %d", &b, &h);
+  printf("%d\n", shape_type(b, h));
+
+  return 0;
+}
